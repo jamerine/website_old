670e66995c8bcabda51ab59a75d42733fb9b6834
jamerine <jamerine@qwik.io>
2015-11-25 21:32:28 +0000

    Finish static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,19 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Qwik.io"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Qwik.io"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Qwik.io"
   end
 
 end
