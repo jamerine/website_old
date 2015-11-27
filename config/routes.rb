d972da08f397e1e4ac86464942ee9df95ed00389
jamerine <jamerine@qwik.io>
2015-11-27 16:39:12 +0000

    Add Creative Start Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'creatives#index'
 
-  get 'pages/about'
+  #get 'pages/about'
 
-  get 'pages/contact'
-
+  #get 'pages/contact'
+  #get 'creatives/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Qwik.io"
-  end
+  #test "should get home" do
+  #  get :index
+  #  assert_response :success
+  #  assert_select "title", "Home | Qwik.io"
+  #end
 
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Qwik.io"
-  end
+  #test "should get about" do
+  #  get :about
+  #  assert_response :success
+  #  assert_select "title", "About | Qwik.io"
+  #end
 
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | Qwik.io"
-  end
+  #test "should get contact" do
+  #  get :contact
+  #  assert_response :success
+  #  assert_select "title", "Contact | Qwik.io"
+  #end
 
 end
